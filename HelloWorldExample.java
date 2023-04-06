88b14b5284fe6364e70f867ba55e9a9ea4ee4d34
Asudheer4013 <37829445+Asudheer4013@users.noreply.github.com>
2023-04-06 20:57:56 +0530

    Update HelloWorldExample.java

--- HelloWorldExample.java
+++ HelloWorldExample.java
@@ -1,11 +1,6 @@
 import java.lang.*;
 public class HelloWorldExample{
- 
   public static void main(String args[]){
- 
-  
-    System.out.println("sravanthi    reddy");
-  
+    System.out.println("sravanthi reddy");
   }
- 
 }
